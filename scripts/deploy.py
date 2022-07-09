a7a5223e610bc332e0474ca206e8c3cf562940c2
Yamamuchi <harveyyorke03@icloud.com>
2022-07-09 17:13:12 +0100

    Interact with contract on local ganache chain

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,14 +1,25 @@
-from brownie import accounts, config
+from lib2to3.pgen2.literals import simple_escapes
+from brownie import accounts, config, SimpleStorage
 
 
 def deploy_simple_storage():
-    # account = accounts[0]
-    # print(account)
+    account = accounts[0]
 
     # account = accounts.load("yamamuchi")
-    # print(account)
 
-    account = accounts.add(config["wallets"]["from_key"])
+    # account = accounts.add(config["wallets"]["from_key"])
+
+    simple_storage = SimpleStorage.deploy({"from": account})
+    print(f"Deployed contract: {simple_storage}")
+
+    stored_value = simple_storage.retrieve()
+    print(f"Stored value: {stored_value}")
+
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+
+    updated_stored_value = simple_storage.retrieve()
+    print(f"Updated stored value: {updated_stored_value}")
 
 
 def main():
