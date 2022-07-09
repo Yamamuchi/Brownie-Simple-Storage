a183f033893784c5deb2a45b134dd727df267b15
Yamamuchi <harveyyorke03@icloud.com>
2022-07-09 15:00:29 +0100

    Reconfigured brownie to retrieve private key from .env

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,8 +5,10 @@ def deploy_simple_storage():
     # account = accounts[0]
     # print(account)
 
-    account = accounts.load("yamamuchi")
-    print(account)
+    # account = accounts.load("yamamuchi")
+    # print(account)
+
+    account = accounts.add(os.getenv("PRIVATE_KEY"))
 
 
 def main():
