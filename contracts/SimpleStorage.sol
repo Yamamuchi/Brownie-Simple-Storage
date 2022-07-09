87e8bbd2672385246fd27fe7a29f89d6450ec3bf
Yamamuchi <harveyyorke03@icloud.com>
2022-07-09 17:14:37 +0100

    Solidity smart contract to be deployed

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -5,7 +5,6 @@ pragma solidity >=0.6.0 <0.9.0;
 contract SimpleStorage {
     uint256 favoriteNumber;
 
-    // This is a comment!
     struct People {
         uint256 favoriteNumber;
         string name;
